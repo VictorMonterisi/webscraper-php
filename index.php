91a982865f824f827841d1d2a88df5b2fa616564
Victor Monterisi <victoraugustobm1@gmail.com>
2023-08-15 22:41:30 -0300

    Primeiro refinamento do código

--- index.php
+++ index.php
@@ -1,6 +1,22 @@
 <?php
-    $conteudo = file_get_contents('https://meu-cv.vercel.app/api/');
 
-    preg_match_all('/<ul class="tecnologias">(.*?)<span>(.*?)<\/span>(.*?)  <\/ul>/s', $conteudo, $matches);
+function scraper() {
+    $conteudo = file_get_contents('https://meu-cv.vercel.app/api/'); // Faz uma requisição GET para encontrar a webpage que será analisada e retorna o seu corpo
+    
+    if($conteudo === false) {
+        echo 'Erro ao obter o conteúdo da página.';
+        
+    } else {
+        preg_match_all('/<ul class="tecnologias">(.*?)<\/ul>/s', $conteudo, $matches); // Busca todos os termos encontrados na lista Tecnologias e armazena no array $matches
 
-    echo strip_tags($matches[0][0]);
+        if(isset($matches[0])) {
+            foreach($matches[0] as $match) {
+                echo $match;
+            }
+        } else {
+            echo 'Nenhuma correspondência encontrada';
+        }
+    }
+    
+}
+scraper();
