ae6b0c2b62789f0dc2259650e18e68070fa1362f
Victor Monterisi <victoraugustobm1@gmail.com>
2023-08-16 09:15:26 -0300

    Ajustes e adição de um estilo simples

---
+++ includes/funcao-webscraper.php
@@ -0,0 +1,21 @@
+<?php
+
+    function webscraper() {
+        $conteudo = file_get_contents('https://meu-cv.vercel.app/api/'); // Faz uma requisição GET para encontrar a webpage que será analisada e retorna o seu corpo
+
+        if($conteudo === false) {
+            echo 'Erro ao obter o conteúdo da página.';
+
+        } else {
+            preg_match_all('/<ul class="tecnologias">(.*?)<\/ul>/s', $conteudo, $matches); // Busca todos os termos encontrados na lista Tecnologias e armazena no array $matches
+
+            if(isset($matches[0])) {
+                foreach($matches[0] as $match) {
+                    echo $match;
+                }
+            } else {
+                echo 'Nenhuma correspondência encontrada';
+            }
+        }
+
+    }
--- index.php
+++ index.php
@@ -1,22 +1,16 @@
-<?php
-
-function scraper() {
-    $conteudo = file_get_contents('https://meu-cv.vercel.app/api/'); // Faz uma requisição GET para encontrar a webpage que será analisada e retorna o seu corpo
-    
-    if($conteudo === false) {
-        echo 'Erro ao obter o conteúdo da página.';
-        
-    } else {
-        preg_match_all('/<ul class="tecnologias">(.*?)<\/ul>/s', $conteudo, $matches); // Busca todos os termos encontrados na lista Tecnologias e armazena no array $matches
-
-        if(isset($matches[0])) {
-            foreach($matches[0] as $match) {
-                echo $match;
-            }
-        } else {
-            echo 'Nenhuma correspondência encontrada';
-        }
-    }
-    
-}
-scraper();
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Webscraper</title>
+    <script src="https://kit.fontawesome.com/d1ab779ca2.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <?php 
+        include "includes/funcao-webscraper.php";
+        webscraper();
+    ?>
+</body>
+</html>
